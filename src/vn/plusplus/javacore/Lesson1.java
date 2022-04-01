7d7833c1f529087170a6f2643fcd827f262b4640
KiemNX <kiemnguyenxuan@gmail.com>
2022-04-01 20:59:41 +0700

    Init project

---
+++ src/vn/plusplus/javacore/Lesson1.java
@@ -0,0 +1,11 @@
+package vn.plusplus.javacore;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("First line");
+
+        System.out.println("Second line");
+
+
+    }
+}
---
+++ src/vn/plusplus/javacore/lesson1/Example1.java
@@ -0,0 +1,7 @@
+package vn.plusplus.javacore.lesson1;
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("First line example 1");
+    }
+}
